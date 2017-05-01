991e6176bf2b85adedc0bb6d885462b946cc6703
Ethan Turkeltaub <ethan@oakmade.com>
2017-05-01 10:47:07 -0400

    Added Problem2 and 2 other files

---
+++ _uploads/Problem2.java
@@ -0,0 +1,37 @@
+
+public class Problem2 {
+
+	public static double findmaxdiff(double[] A){
+		// Complete the method to find the maximum difference of a given array A
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Test your findmaxdiff() method here
+		
+		double[] testarray1 = {2, 3, 10, 6, 4, 8, 1};
+		// maxdiff: 8
+		
+		double[] testarray2 = {7, 9, 5, 6, 3, 2};
+		// maxdiff: 2
+		
+		System.out.println("The maxdiff is " + findmaxdiff(testarray1));
+		
+		System.out.println("The maxdiff of testarray2 is " + findmaxdiff(testarray2));
+		
+		
+		
+		
+		
+		
+	}
+
+}
